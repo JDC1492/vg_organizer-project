2a8be20d57beb03b9963da130b64c042eea2dc88
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-10-05 17:07:44 -0400

    rearranged the order of my routes so the new route wouldn't get interrupted by the ':id' route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,13 +5,24 @@ class GamesController < ApplicationController
         erb :'games/index'
     end
 
+    get '/games/new' do
+        erb :'games/new'
+    end
+
     get '/games/:id' do 
+        # binding.pry
         @game = Game.find_by_id(params[:id])
         erb :'games/show'
     end
 
-    get '/games/new' do
-        
+    post '/games' do
+        post = Post.new(params)
+        if post.save
+            redirect "/games/#{post.id}"
+        else
+            redirect "/games/new"
+        end
+
     end
 
 
