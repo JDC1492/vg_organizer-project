6939902464d5e1cd81e79509afcdd91057bedca3
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-10-05 10:52:35 -0400

    finalized first migration, added seed data to fill in created table, created model

--- db/migrate/20201005133035_create_games_table.rb
+++ db/migrate/20201005133035_create_games_table.rb
@@ -2,6 +2,7 @@ class CreateGamesTable < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.string :title
+      t.string :console
       t.string :genre
       t.string :description
       t.boolean :complete, default: false
