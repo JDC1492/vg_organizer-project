61186c453912de48224d86ae61b55fb7f88b1f0b
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-10-06 12:06:01 -0400

    fixed order in gamescontroller to allow my new form to be submited and shown

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,19 +9,20 @@ class GamesController < ApplicationController
         erb :'games/new'
     end
 
+    get '/games/:id' do 
+        # binding.pry
+        @game = Game.find_by_id(params[:id])
+        erb :'games/show'
+    end
+    
     post '/games' do
+        binding.pry
         new_game=Game.new(params)
         if new_game.save
             redirect "games/#{new_game.id}"
         else
             redirect 'games/new'
         end
-    end
-
-    get '/games/:id' do 
-        # binding.pry
-        @game = Game.find_by_id(params[:id])
-        erb :'games/show'
     end
 
     get '/games/:id/edit' do
